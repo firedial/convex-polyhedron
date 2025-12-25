aaad9247f3198edad4a9b7db82bfeebb3f4e6996
firedial <42674772+firedial@users.noreply.github.com>
2025-12-25 21:15:57 +0900

    JSON 出力まで

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
 import Triangle
@@ -5,6 +7,11 @@ import Point
 import Face
 import Polyhedron
 import ConvexPolyhedron
+import Character
+import AlgUtil
+import Data.Aeson
+import GHC.Generics
+import qualified Data.ByteString.Lazy.Char8 as BL
 
 {-
 整面凸多面体であるということ
@@ -37,15 +44,24 @@ import ConvexPolyhedron
 * 体積
 * 体積(1辺1)
 * 頂点形状と余角
-* 辺形状と2面角
+* 辺形状と2面角 <- これは最初は求めない
 -}
 
+data OutputData = OutputData
+    { a :: [Int]
+    , b :: Int
+    } deriving (Generic, Show)
+
+instance ToJSON OutputData
 
 main :: IO ()
 -- main = print $ (tan11_25 - sin11_25 / cos11_25)
 -- main = print $ isSamePlain (Point sin15 cos30 43) (Point sin30 cos15 34) (Point sin11_25 cos15 2) (Point sin15 cos9 3)
 -- main = print $ isSamePlain (Point 1 1 1) (Point 2 2 2) (Point 3 3 3) (Point 4 4 4)
 -- main = print $ isRegularFace $ Face [(Point 0 0 0), (Point 0 0 1), (Point 0 1 1), (Point 0 1 0)]
-main = print $ isConvexPolyhedron r1
+-- main = print $ isConvexPolyhedron r1
+-- main = print $ toValue $ volume (pfaces r1)
+main = BL.putStrLn $ encode $ OutputData [1, 3] 6
+-- main = print $ toValue 1
 -- main = print $ pfaces r1
 
